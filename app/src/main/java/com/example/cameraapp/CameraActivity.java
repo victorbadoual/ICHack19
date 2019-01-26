fb4f41bfdf45ac8ee06cba6d47b7d2377561abf9
Tiger Cross <tc2017@ic.ac.uk>
2019-01-26 14:23:47 +0000

    Begin modifications to camera interface

--- app/src/main/java/com/example/cameraapp/CameraActivity.java
+++ app/src/main/java/com/example/cameraapp/CameraActivity.java
@@ -1,6 +1,46 @@
 package com.example.cameraapp;
 
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CaptureRequest;
+import android.media.ImageReader;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Size;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.SurfaceView;
+import android.widget.Button;
 
 public class CameraActivity extends AppCompatActivity {
+
+    private static final String HEADER = "Original Photo";
+
+    private Button takePictureButton;
+    private SurfaceView surfaceView;
+
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
+
+    private String cameraId;
+    protected CameraDevice cameraDevice;
+    protected CameraCaptureSession cameraCaptureSessions;
+    protected CaptureRequest captureRequest;
+    protected CaptureRequest.Builder captureRequestBuilder;
+    private Size imageDimension;
+    private ImageReader imageReader;
+
+//    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_camera);
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction().replace(R.id.container,
+//                    CameraFragment.newInstance()).commit();
+//        }
+//    }
 }
